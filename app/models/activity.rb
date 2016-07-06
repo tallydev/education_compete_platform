214a23c6d6f5d8a45d80a81f69424428eee2d788
shtzr840329 <shtzr840329@gmail.com>
2016-07-06 20:18:23 +0800

    fix activity file merge error

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -2,7 +2,6 @@
 #
 # Table name: activities
 #
-<<<<<<< HEAD
 #  id         :integer          not null, primary key
 #  name       :string(255)      not null
 #  start_time :datetime
@@ -11,22 +10,6 @@
 #  updated_at :datetime         not null
 #  status     :integer          default(0)
 #  type       :string(255)
-=======
-#  id          :integer          not null, primary key
-#  name        :string           not null
-#  start_time  :datetime
-#  end_time    :datetime
-#  created_at  :datetime         not null
-#  updated_at  :datetime         not null
-#  status      :integer          default(0)
-#  type        :string
-#  bulletin_id :integer
-#  short_name  :string
-#
-# Indexes
-#
-#  index_activities_on_bulletin_id  (bulletin_id)
->>>>>>> e5f86b40d573938d062cd407ec5cf1f19dc1a786
 #
 
 class Activity < ActiveRecord::Base
