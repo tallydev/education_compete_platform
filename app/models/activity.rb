53076cbe4667126d151fd2d34b0ca9d34f27f5ef
liyijie <liyijie825@gmail.com>
2016-07-06 12:44:51 +0800

    add type to Activity model

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -9,6 +9,7 @@
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
 #  status     :integer          default(0)
+#  type       :string
 #
 
 class Activity < ActiveRecord::Base
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160706043949) do
+ActiveRecord::Schema.define(version: 20160706044442) do
 
   create_table "activities", force: :cascade do |t|
     t.string   "name",                   null: false
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 20160706043949) do
     t.datetime "created_at",             null: false
     t.datetime "updated_at",             null: false
     t.integer  "status",     default: 0
+    t.string   "type"
   end
 
   create_table "bulletins", force: :cascade do |t|
