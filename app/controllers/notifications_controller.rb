b74be27666bb2f1fe644943133b4d75e208bfe8e
liyijie <liyijie825@gmail.com>
2016-07-06 16:51:28 +0800

    add notification controller

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -1,9 +1,10 @@
 class NotificationsController < ApplicationController
 	def index
-		
+    @notification = Bulletin.all
 	end
 
 	def show
-		
+    @notification = Bulletin.find(params[:id])
+    @activity = @notification.try(:activity)
 	end
 end
