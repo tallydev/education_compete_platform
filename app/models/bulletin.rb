1baad8b54d9f33c7a3bda41928d36b274ea5b169
shtzr840329 <shtzr840329@gmail.com>
2016-07-06 10:28:33 +0800

    add school seed

--- app/models/bulletin.rb
+++ app/models/bulletin.rb
@@ -1,2 +1,18 @@
+# == Schema Information
+#
+# Table name: bulletins
+#
+#  id          :integer          not null, primary key
+#  title       :string(255)
+#  content     :text(65535)
+#  activity_id :integer
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+# Indexes
+#
+#  index_bulletins_on_activity_id  (activity_id)
+#
+
 class Bulletin < ActiveRecord::Base
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160705160831) do
+ActiveRecord::Schema.define(version: 20160705161548) do
 
   create_table "activities", force: :cascade do |t|
     t.string   "name",       limit: 255,             null: false
@@ -21,6 +21,16 @@ ActiveRecord::Schema.define(version: 20160705160831) do
     t.datetime "updated_at",                         null: false
     t.integer  "status",     limit: 4,   default: 0
   end
+
+  create_table "bulletins", force: :cascade do |t|
+    t.string   "title",       limit: 255
+    t.text     "content",     limit: 65535
+    t.integer  "activity_id", limit: 4
+    t.datetime "created_at",                null: false
+    t.datetime "updated_at",                null: false
+  end
+
+  add_index "bulletins", ["activity_id"], name: "index_bulletins_on_activity_id", using: :btree
 
   create_table "departments", force: :cascade do |t|
     t.string   "name",       limit: 255
