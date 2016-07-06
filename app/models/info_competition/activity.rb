a62d6e2621d1790e80450bbc133c47b0782c4338
liyijie <liyijie825@gmail.com>
2016-07-06 18:14:07 +0800

    fix confilct

--- app/models/info_competition/activity.rb
+++ app/models/info_competition/activity.rb
@@ -2,16 +2,6 @@
 #
 # Table name: activities
 #
-<<<<<<< b74be27666bb2f1fe644943133b4d75e208bfe8e
-#  id         :integer          not null, primary key
-#  name       :string(255)      not null
-#  start_time :datetime
-#  end_time   :datetime
-#  created_at :datetime         not null
-#  updated_at :datetime         not null
-#  status     :integer          default(0)
-#  type       :string(255)
-=======
 #  id          :integer          not null, primary key
 #  name        :string           not null
 #  start_time  :datetime
@@ -25,7 +15,6 @@
 # Indexes
 #
 #  index_activities_on_bulletin_id  (bulletin_id)
->>>>>>> add activities seed
 #
 
 class InfoCompetition::Activity < Activity
