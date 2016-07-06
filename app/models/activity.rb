145e3d83462974056c8d30d070a58da9086f019f
iboying <weboying@gmail.com>
2016-07-06 20:31:55 +0800

    code Recruit new page

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -3,13 +3,13 @@
 # Table name: activities
 #
 #  id         :integer          not null, primary key
-#  name       :string(255)      not null
+#  name       :string           not null
 #  start_time :datetime
 #  end_time   :datetime
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
 #  status     :integer          default(0)
-#  type       :string(255)
+#  type       :string
 #
 
 class Activity < ActiveRecord::Base
