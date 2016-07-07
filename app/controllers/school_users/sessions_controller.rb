795b4da1686b0446bfaed4aace0ecf8d9baf7210
zhangliugang <www.zhangyi508@gmail.com>
2016-07-08 03:20:19 +0800

    fix nav

--- app/controllers/school_users/sessions_controller.rb
+++ app/controllers/school_users/sessions_controller.rb
@@ -1,5 +1,5 @@
 class SchoolUsers::SessionsController < Devise::SessionsController
-before_action :configure_sign_in_params, only: [:create]
+# before_action :configure_sign_in_params, only: [:create]
 
   # GET /resource/sign_in
   # def new
@@ -7,10 +7,9 @@ before_action :configure_sign_in_params, only: [:create]
   # end
 
   # POST /resource/sign_in
-  def create
-    p "==========#{params}==========="
-    super
-  end
+  # def create
+  #   super
+  # end
 
   # DELETE /resource/sign_out
   # def destroy
@@ -20,7 +19,7 @@ before_action :configure_sign_in_params, only: [:create]
   # protected
 
   # If you have extra params to permit, append them to the sanitizer.
-  def configure_sign_in_params
-    devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:phone, :password, :password_confirmation, :name) }
-  end
+  # def configure_sign_in_params
+  #   devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:phone, :password, :password_confirmation, :name) }
+  # end
 end
