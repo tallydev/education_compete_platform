d05d8ab4aed69a63a20f896e4f722fd281aac388
liyijie <liyijie825@gmail.com>
2016-07-16 20:45:44 +0800

    update the opus controller

--- app/controllers/info_competition/opuses_controller.rb
+++ app/controllers/info_competition/opuses_controller.rb
@@ -19,14 +19,11 @@ class InfoCompetition::OpusesController < ApplicationController
   #   respond_with(@info_competition_opus)
   # end
 
-  # def edit
-  # end
-
-  # def create
-  #   @info_competition_opus = InfoCompetition::Opus.new(info_competition_opus_params)
-  #   @info_competition_opus.save
-  #   respond_with(@info_competition_opus)
-  # end
+  def create
+    @info_competition_opus = InfoCompetition::Opus.new(info_competition_opus_params)
+    @info_competition_opus.save
+    respond_with(@info_competition_opus)
+  end
 
   def new
     @first_opus = current_player.recruits.blank? ? nil : current_player.recruits.first
@@ -40,32 +37,6 @@ class InfoCompetition::OpusesController < ApplicationController
   def edit
   end
 
-  def create
-    @info_competition_recruit = InfoCompetition::Recruit.find(params[:recruit_id])
-    @info_competition_opus = @info_competition_recruit.build_opus(info_competition_opus_params)
-    # @info_competition_opus.save
-
-    @info_competition_opus.media = Media.new if @info_competition_opus.media.blank?
-    @info_competition_opus.media.file = params[:info_competition_opus][:madia]
-
-    @media = @info_competition_opus.build_media(media_params)
-
-    if @info_competition_opus.save && @media.save
-      respond_with(@info_competition_opus)
-      p "============================="
-    else
-      return render action: :new
-      p "++++++++++++++++++++++++++++++"
-    end
-
-    
-
-    # @note = @info_competition_opus.build_note(note_params)
-    # @note.save
-
-    # @ppt = @info_competition_opus.build_ppt(ppt_params)
-    # @ppt.save
-  end
 
   def update
     @info_competition_opus.update(info_competition_opus_params)
@@ -83,23 +54,12 @@ class InfoCompetition::OpusesController < ApplicationController
     end
 
     def info_competition_opus_params
-      params.require(:info_competition_opus)
-          .permit(:recruit_id)
-    end
-
-    def media_params
-      params[:info_competition_opus][:media]
-          .permit(:file)
-      
-    end
-    def plan_params
-      params[:info_competition_opus][:plan].permit(:file)
-    end
-    def note_params
-      params[:info_competition_opus][:note].permit(:file)
-    end
-    def ppt_params
-      params[:info_competition_opus][:ppt].permit(:file)
+      params.require(:info_competition_opus).permit(:recruit_id, 
+            media_attributes: [:id, :file, :_destroy],
+            plan_attributes: [:id, :file, :_destroy],
+            ppt_attributes: [:id, :file, :_destroy],
+            note_attributes: [:id, :file, :_destroy],
+            )
     end
 
 end
