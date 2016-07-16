8863f111270e343e116e45402d545acc3bec38eb
iboying <weboying@gmail.com>
2016-07-17 00:22:05 +0800

    complete home

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,7 +4,7 @@ class HomeController < ApplicationController
     @notifications = Bulletin.all
     @activities = Activity.all
 
-    @activity_one = Activity.find(1).left_days
-    @activity_two = Activity.find(2).left_days
+    @one_days = Activity.find(1).left_days
+    @two_days = Activity.find(2).left_days
   end
 end
