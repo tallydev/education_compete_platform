53f3bd5d9f56398aa5bdcf5ee685c25ca9800431
zhangliugang <www.zhangyi508@gmail.com>
2016-07-16 23:38:20 +0800

    fix

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,5 +3,8 @@ class HomeController < ApplicationController
   def index
     @notifications = Bulletin.all
     @activities = Activity.all
+
+    @activity_one = Activity.find(1).left_days
+    @activity_two = Activity.find(2).left_days
   end
 end
