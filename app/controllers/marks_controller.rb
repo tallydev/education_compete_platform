49fea14217c7e8a454fbb18c16e235d4d0c428a3
zhangliugang <www.zhangyi508@gmail.com>
2016-08-18 11:20:07 +0800

    fix  bug

--- app/controllers/marks_controller.rb
+++ app/controllers/marks_controller.rb
@@ -1,40 +1,29 @@
 class MarksController < ApplicationController
-  before_action :set_mark, only: [:update]
   before_action :authenticate_expert!, only: [:update]
   respond_to :html, :js
 
   def update
     array = params[:itemArray][0].split(",").map { |item| item.to_i }
     if params[:type] == "info"
+      @markInfo = InfoCompetition::Opus.find(params[:opus_id]).recruit.marks.where(expert_id: current_expert.id).first
+      @opusInfo = InfoCompetition::Opus.find(params[:opus_id])
       if @markInfo.update(item_array: array)
         flash[:success] = "评分成功"
-        p @markInfo
-        p "======================================"
         respond_with @opusInfo
       else
         flash[:error] = "评分失败！"
         respond_with @opusInfo
       end
     elsif params[:type] == "talk"
+      @markTalk = TalkCompetition::Opus.find(params[:opus_id]).recruit.marks.where(expert_id: current_expert.id).first
+      @opusTalk = TalkCompetition::Opus.find(params[:opus_id])
       if @markTalk.update(item_array: array)
         flash[:success] = "评分成功"
-        p @markTalk
-        p "======================================"
         respond_with @opusTalk
       else
         flash[:error] = "评分失败！"
         respond_with @opusTalk
       end
     end
-    p "++++++++++++++++++++++++++"
-    p params[:type]
-  end
-
-  private
-  def set_mark
-    @markInfo = InfoCompetition::Opus.find(params[:opus_id]).recruit.marks.where(expert_id: current_expert.id).first
-    @opusInfo = InfoCompetition::Opus.find(params[:opus_id])
-    @markTalk = TalkCompetition::Opus.find(params[:opus_id]).recruit.marks.where(expert_id: current_expert.id).first
-    @opusTalk = TalkCompetition::Opus.find(params[:opus_id])
   end
 end
