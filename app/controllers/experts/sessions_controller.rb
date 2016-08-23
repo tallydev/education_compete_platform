800f4cb6e96f2177dc7047941579360d0dd27d24
zhangliugang <www.zhangyi508@gmail.com>
2016-08-24 02:40:49 +0800

    fix typhoon mark

--- app/controllers/experts/sessions_controller.rb
+++ app/controllers/experts/sessions_controller.rb
@@ -2,9 +2,12 @@ class Experts::SessionsController < Devise::SessionsController
 # before_action :configure_sign_in_params, only: [:create]
 
   # GET /resource/sign_in
-  # def new
-  #   super
-  # end
+  def new
+    super
+     if params[:from] == "phone"
+      render layout: "phone"
+    end
+  end
 
   # POST /resource/sign_in
   # def create
