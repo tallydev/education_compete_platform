f45dc87ee336dacc2c1004e40d68289560ce04cb
zhangliugang <www.zhangyi508@gmail.com>
2016-08-25 01:41:58 +0800

    print xsl

--- app/controllers/admin/marks_controller.rb
+++ app/controllers/admin/marks_controller.rb
@@ -4,6 +4,14 @@ class Admin::MarksController < Admin::BaseController
 
 	def index
 		@recruits = @activity.project_recruits(params[:project]).avg_mark_desc
+		respond_to do |format|
+        format.xls {
+            send_data( xls_content_for(@recruits),
+                :type => "text/excel;charset=utf-8; header=present",
+                :filename => "#{@activity.name}.xls" )
+        }
+        format.html
+    end
 	end
 
 	# 分配一个报名的评委专家
@@ -46,5 +54,36 @@ class Admin::MarksController < Admin::BaseController
 		def set_activity
 			@activity = Activity.find(params[:activity_id])
 		end
+
+		def xls_content_for(objs)
+      xls_report = StringIO.new
+      book = Spreadsheet::Workbook.new
+      sheet1 = book.create_worksheet :name => "成绩统计"
+
+      blue = Spreadsheet::Format.new :color => :blue, :weight => :bold, :size => 10
+      sheet1.row(0).default_format = blue
+
+      sheet1.row(0).concat %w{排名 平均分 评委人数 姓名 院校 作品名称 评委评分}
+      count_row = 1
+      objs.each_with_index do |obj, index|
+        sheet1[count_row, 0] = index+1
+        sheet1[count_row, 1] = obj.avg_score
+        sheet1[count_row, 2] = obj.score_count
+        sheet1[count_row, 3] = obj.try(:player_info).try(:name)
+        sheet1[count_row, 4] = obj.try(:school).try(:name)
+        sheet1[count_row, 5] = obj.name
+        col_marks = []
+        obj.score_marks.each_with_index do |mark,index|
+        	col_marks.push("#{mark.expert.phone} : #{mark.score.present? ? mark.score : 0}分")
+        end
+        str = ""
+        col_marks.reverse_each { |col_mark| str += "    #{col_mark}    ||" }
+        sheet1[count_row, 6] = str
+        count_row += 1
+      end
+
+      book.write xls_report
+      xls_report.string
+    end
 
 end
