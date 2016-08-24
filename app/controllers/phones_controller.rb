17fea15710f624a3c92ec831827b15ad967825f7
iboying <weboying@gmail.com>
2016-08-24 12:51:42 +0800

    complete with phone index

--- app/controllers/phones_controller.rb
+++ app/controllers/phones_controller.rb
@@ -6,6 +6,7 @@ class PhonesController < ApplicationController
 
 	# 选择某个线下比赛后跳转，选手列表页（手机端选择比赛后的主页）
 	def index
+		# 获取的为线下的比赛
 		_activity = Activity.find(params[:activity_id])
 		@recruits_offline = _activity.recruits
 	end
