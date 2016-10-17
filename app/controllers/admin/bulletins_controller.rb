a7249f08eea89c5e30764215722c4e0e43bb0d57
liyijie <liyijie825@gmail.com>
2016-10-17 14:54:43 +0800

    update bulletins controller

--- app/controllers/admin/bulletins_controller.rb
+++ app/controllers/admin/bulletins_controller.rb
@@ -1,6 +1,9 @@
 module Admin
   class BulletinsController < BaseController
-    protect_from_forgery :except => :create  
+    protect_from_forgery :except => [:create, :update]  
+    before_action :set_bulletin, only: [:show, :update, :edit, :destroy]
+
+    respond_to :html, :js
 
     def index
       @bulletins = Bulletin.all.paginate(:page => params[:page], :per_page => 20)
@@ -21,7 +24,33 @@ module Admin
       end
     end
 
+    def edit
+      respond_with @bulletin
+    end
+
+    def update
+      if @bulletin.update(bulletin_params)
+        flash[:success] = "修改成功"
+        respond_to do |format|
+          format.js { render js: "location.href = '#{admin_bulletins_path}'" }
+        end
+      else
+        flash[:danger] = "修改失败"
+        render :new
+      end
+    end
+
+    def destroy
+      @bulletin.destroy
+      redirect_to admin_bulletins_path
+    end
+
     private
+
+    def set_bulletin
+      @bulletin = Bulletin.find(params[:id])
+    end
+
     def bulletin_params
       params.require(:bulletin).permit(:title, :content, :activity_id, paper_attributes: [:id, :file, :_destroy] )
     end
