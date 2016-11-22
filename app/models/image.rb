05e411330687c2bf6a1793931be1db5039800bf5
tangmingchao <18798805737@139.com>
2016-11-22 16:14:11 +0800

    rdit image.rb

--- app/models/image.rb
+++ app/models/image.rb
@@ -21,15 +21,18 @@
 
 class Image < ActiveRecord::Base
   belongs_to :imageable, polymorphic: true
-
+  # paperclip gem
   has_attached_file :file, styles: { mini: '48x48>', small: '150x150>', medium: '300x300>', product: '600x600>', large: '1280x1280>' }
 
   validates_attachment_presence :file
   validates_attachment_size     :file, less_than: 5.megabytes
   validates_attachment_content_type :file, content_type: /image\/.*\Z/
 
-  def url mode
+  def url mode=:medium
     file.present? ? file.url(mode) : ""
   end
-  
+
+  def image_url mode=:medium
+    ActionController::Base.helpers.image_url( url(mode) ) || ""
+  end
 end
