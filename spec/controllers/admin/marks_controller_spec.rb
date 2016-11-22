e701d5ea52692322b27909ca0b67a6fc1c55701a
tangmingchao <18798805737@139.com>
2016-11-22 15:43:01 +0800

    setting spec

--- spec/controllers/admin/marks_controller_spec.rb
+++
@@ -1,5 +0,0 @@
-require 'rails_helper'
-
-RSpec.describe Admin::MarksController, type: :controller do
-
-end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -38,7 +38,9 @@ RSpec.configure do |config|
   # RSpec Rails can automatically mix in different behaviours to your tests
   # based on their file location, for example enabling you to call `get` and
   # `post` in specs under `spec/controllers`.
-  #
+  ################################################################
+  Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
+  ################################################################
   # You can disable this behaviour by removing the line below, and instead
   # explicitly tag your specs with their type, e.g.:
   #
@@ -55,3 +57,11 @@ RSpec.configure do |config|
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
 end
+#######################################
+Shoulda::Matchers.configure do |config|
+  config.integrate do |with|
+    with.test_framework :rspec
+    with.library :rails
+  end
+end
+#######################################
