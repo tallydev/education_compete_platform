e7167604df6521a6f1d39d7b2b998c200e7ea956
tangmingchao <18798805737@139.com>
2016-11-26 16:57:54 +0800

    edit manager_feedback#model and controller

--- app/controllers/manager_feedbacks_controller.rb
+++ app/controllers/manager_feedbacks_controller.rb
@@ -1,47 +1,63 @@
 class ManagerFeedbacksController < ApplicationController
-  before_action :set_manager_feedback, only: [:show, :edit, :update, :destroy]
+  # before_action :authenticate_user!
+  before_action :set_training_course
+  before_action :set_player_feedback, only: [:create, :update]
 
-  respond_to :html
+  before_action :set_manager_feedback, only: [:show]
 
-  def index
-    @manager_feedbacks = ManagerFeedback.all
+  respond_to :html, :json
+
+  def index #####
+    page = params[:page] || 1
+    per_page = params[:per_page] || 15
+    @manager_feedbacks = ManagerFeedback.all.paginate(page: page, per_page: per_page)
     respond_with(@manager_feedbacks)
   end
 
-  def show
+  def show####
     respond_with(@manager_feedback)
-  end
-
-  def new
-    @manager_feedback = ManagerFeedback.new
-    respond_with(@manager_feedback)
-  end
-
-  def edit
   end
 
   def create
-    @manager_feedback = ManagerFeedback.new(manager_feedback_params)
-    @manager_feedback.save
-    respond_with(@manager_feedback)
+    if @manager_feedback.blank?
+      @manager_feedback = @training_course.build_manager_feedback
+      @manager_feedback.save
+      respond_with(@manager_feedback)
+    else
+      @error = "反馈信息创建失败"
+      respond_with @error, template: "error"
+    end
   end
 
   def update
-    @manager_feedback.update(manager_feedback_params)
-    respond_with(@manager_feedback)
+    if @manager_feedback.update(manager_feedback_params)
+      respond_with(@manager_feedback)
+    else
+      @error = "反馈信息创建失败"
+      respond_with @error, template: "error"
+    end
   end
 
-  def destroy
-    @manager_feedback.destroy
-    respond_with(@manager_feedback)
-  end
+  # def destroy
+  #   @manager_feedback.destroy
+  #   respond_with(@manager_feedback)
+  # end
 
   private
     def set_manager_feedback
       @manager_feedback = ManagerFeedback.find(params[:id])
     end
 
+    def set_training_course
+      @training_course = TrainingCourse.find(params[:training_course_id])
+    end
+
+    def set_manager_feedback
+      @manager_feedback = @training_course.manager_feedback
+    end
+
     def manager_feedback_params
-      params[:manager_feedback]
+      params.require(:manager_feedback).permit(:training_course_id, :admin_id, :organizer, :total_hours, :total_expenses,
+                                     :total_expenses_info, :feedback, :remark)
     end
 end
--- app/models/manager_feedback.rb
+++ app/models/manager_feedback.rb
@@ -26,4 +26,19 @@
 #
 
 class ManagerFeedback < ActiveRecord::Base
+  belongs_to :admin
+  belongs_to :training_course
+
+  #搜索功能
+  scope :keyword, -> (keyword) do
+    return all if keyword.blank?
+    joins(:admin).joins(:training_course).where(
+      'admins.name LIKE ?
+      OR manager_feedbacks.organizer LIKE ?
+      OR training_courses.name LIKE ?',
+      "%#{keyword}%",
+      "%#{keyword}%",
+      "%#{keyword}%"
+    )
+  end
 end
