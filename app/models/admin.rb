15f4d90624687404b29472c1349c2a9937c83624
tangmingchao <18798805737@139.com>
2016-11-28 12:04:35 +0800

    edit admin.rb

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -32,5 +32,6 @@ class Admin < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable,
+         authentication_keys: [:email]
 end
