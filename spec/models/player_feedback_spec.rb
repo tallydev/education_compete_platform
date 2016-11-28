8217e90c7bd8d7e7b456b2b6b0f809e923f36f4b
tangmingchao <18798805737@139.com>
2016-11-28 15:46:04 +0800

    edit player_model

--- spec/models/player_feedback_spec.rb
+++ spec/models/player_feedback_spec.rb
@@ -25,6 +25,6 @@
 require 'rails_helper'
 
 RSpec.describe PlayerFeedback, type: :model do
-  it { should belong_to(:user) }
+  it { should belong_to(:player) }
   it { should belong_to(:training_course) }
 end
