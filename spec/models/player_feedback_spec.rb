916653184c3f55a463c128914adbc398d3e1adc8
tangmingchao <18798805737@139.com>
2016-11-28 14:55:30 +0800

    edit spec/modle

--- spec/models/player_feedback_spec.rb
+++ spec/models/player_feedback_spec.rb
@@ -25,5 +25,6 @@
 require 'rails_helper'
 
 RSpec.describe PlayerFeedback, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should belong_to(:user) }
+  it { should belong_to(:training_course) }
 end
