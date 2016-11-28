5a14821c20a6a3116e3533398dda992bdb70bbcf
tangmingchao <18798805737@139.com>
2016-11-28 14:42:38 +0800

    edit spec/factories/admins.rb

--- spec/factories/admins.rb
+++ spec/factories/admins.rb
@@ -1,5 +1,33 @@
+# == Schema Information
+#
+# Table name: admins
+#
+#  id                     :integer          not null, primary key
+#  email                  :string           default(""), not null
+#  encrypted_password     :string           default(""), not null
+#  reset_password_token   :string
+#  reset_password_sent_at :datetime
+#  remember_created_at    :datetime
+#  sign_in_count          :integer          default(0), not null
+#  current_sign_in_at     :datetime
+#  last_sign_in_at        :datetime
+#  current_sign_in_ip     :string
+#  last_sign_in_ip        :string
+#  created_at             :datetime         not null
+#  updated_at             :datetime         not null
+#  authentication_token   :string(30)
+#
+# Indexes
+#
+#  index_admins_on_authentication_token  (authentication_token) UNIQUE
+#  index_admins_on_email                 (email) UNIQUE
+#  index_admins_on_reset_password_token  (reset_password_token) UNIQUE
+#
+
 FactoryGirl.define do
   factory :admin do
-    
+    email "admin@example.com"
+    password "abcd.1234"
+    authentication_token "qwertyuiop123"
   end
 end
