be6f1797be79869a27e0622ace47626aa01e6abb
tangmingchao <18798805737@139.com>
2016-11-29 17:20:44 +0800

    edit app/controllers/teachers_controller.rb

--- app/controllers/teachers_controller.rb
+++ app/controllers/teachers_controller.rb
@@ -1,10 +1,12 @@
 class TeachersController < ApplicationController
-  before_action :set_teacher, only: [:show, :edit, :update, :destroy]
+  before_action :set_teacher, only: [:show, :update, :destroy]
 
-  respond_to :html
+  respond_to :json
 
   def index
-    @teachers = Teacher.all
+    page = params[:page] || 1
+    per_page = params[:per_page] || 15
+    @teachers = Teacher.all.paginate(page: page, per_page: per_page)
     respond_with(@teachers)
   end
 
@@ -12,18 +14,14 @@ class TeachersController < ApplicationController
     respond_with(@teacher)
   end
 
-  def new
-    @teacher = Teacher.new
-    respond_with(@teacher)
-  end
-
-  def edit
-  end
-
   def create
     @teacher = Teacher.new(teacher_params)
-    @teacher.save
-    respond_with(@teacher)
+    if @teacher.save
+      respond_with(@teacher) 
+    else
+      @error = "老师信息创建 失败 ！"
+      respond_with(@error)
+    end
   end
 
   def update
@@ -42,6 +40,7 @@ class TeachersController < ApplicationController
     end
 
     def teacher_params
-      params[:teacher]
+      params.require(:teacher).permit(:name, :email, :postcode, :mobile, :id_card, :position,
+       :sex, :remark, :phone, :address, :school_id, :job_title, :qq)
     end
 end
