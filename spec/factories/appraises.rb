7ba1463c0f614625427f08ce8636ec342e9af409
tangmingchao <18798805737@139.com>
2016-11-29 14:54:01 +0800

    edit spec/factories/appraises.rb

--- spec/factories/appraises.rb
+++ spec/factories/appraises.rb
@@ -1,5 +1,29 @@
+# == Schema Information
+#
+# Table name: appraises
+#
+#  id                 :integer          not null, primary key
+#  player_id          :integer
+#  teacher_id         :integer
+#  remark             :text(65535)
+#  degree             :string(255)
+#  training_course_id :integer
+#  created_at         :datetime         not null
+#  updated_at         :datetime         not null
+#
+# Indexes
+#
+#  index_appraises_on_player_id           (player_id)
+#  index_appraises_on_teacher_id          (teacher_id)
+#  index_appraises_on_training_course_id  (training_course_id)
+#
+
 FactoryGirl.define do
   factory :appraise do
-    
+    player_id 1
+    teacher_id 1
+    training_course_id 1
+    remark "remark"
+    degree "degree"
   end
 end
