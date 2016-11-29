b65ff95ca88f5f295fe69ac983e1a2c6504c5aef
tangmingchao <18798805737@139.com>
2016-11-29 11:40:56 +0800

    edit app/controllers/admin/manager_feedbacks_controller.rb

--- app/controllers/admin/manager_feedbacks_controller.rb
+++ app/controllers/admin/manager_feedbacks_controller.rb
@@ -1,10 +1,21 @@
 class Admin::ManagerFeedbacksController < ApplicationController
-  before_action :set_admin_manager_feedback, only: [:show, :edit, :update, :destroy]
+  acts_as_token_authentication_handler_for Admin
+  before_action :set_admin_manager_feedback, only: [:show]
 
-  respond_to :html
+  respond_to :json
 
   def index
-    @admin_manager_feedbacks = Admin::ManagerFeedback.all
+    page = params[:page] || 1
+    per_page = params[:per_page] || 15
+    @admin_manager_feedbacks = ManagerFeedback.all.paginate(page: page, per_page: per_page)
+    respond_with(@admin_manager_feedbacks)
+  end
+
+  def list
+    @keyword = params[:keyword].present?? params[:keyword] : " "
+    page = params[:page] || 1
+    per_page = params[:per_page] || 15
+    @admin_manager_feedbacks = ManagerFeedback.keyword(@keyword).paginate(page: page, per_page: per_page)
     respond_with(@admin_manager_feedbacks)
   end
 
@@ -12,36 +23,8 @@ class Admin::ManagerFeedbacksController < ApplicationController
     respond_with(@admin_manager_feedback)
   end
 
-  def new
-    @admin_manager_feedback = Admin::ManagerFeedback.new
-    respond_with(@admin_manager_feedback)
-  end
-
-  def edit
-  end
-
-  def create
-    @admin_manager_feedback = Admin::ManagerFeedback.new(admin_manager_feedback_params)
-    @admin_manager_feedback.save
-    respond_with(@admin_manager_feedback)
-  end
-
-  def update
-    @admin_manager_feedback.update(admin_manager_feedback_params)
-    respond_with(@admin_manager_feedback)
-  end
-
-  def destroy
-    @admin_manager_feedback.destroy
-    respond_with(@admin_manager_feedback)
-  end
-
   private
     def set_admin_manager_feedback
       @admin_manager_feedback = Admin::ManagerFeedback.find(params[:id])
-    end
-
-    def admin_manager_feedback_params
-      params[:admin_manager_feedback]
     end
 end
