2a57491d3f2cd299bd7061190c0762a1ad6acfd8
tangmingchao <18798805737@139.com>
2016-11-29 09:18:36 +0800

    edit  spec/factories/manager_feedbacks.rb

--- spec/factories/manager_feedbacks.rb
+++ spec/factories/manager_feedbacks.rb
@@ -2,13 +2,11 @@ FactoryGirl.define do
   factory :manager_feedback do
     manager_id 1
     training_course_id 1
-    teach "teach"
-    discussion "discussion"
-    visiting "visiting"
-    organization "organization"
-    study_life "study_life"
-    most_value "most_value"
-    most_gain "most_gain"
-    graduate_message "graduate_message" 
+    organizer "organizer"
+    total_hours "total_hours"
+    total_expenses "total_expenses"
+    total_expenses_info "total_expenses_info"
+    feedback "feedback"
+    remark "remark"
   end
 end
