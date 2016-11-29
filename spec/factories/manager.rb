7cb027dba06cd783fd31a04431384c9a1266d43b
tangmingchao <18798805737@139.com>
2016-11-29 10:54:32 +0800

    edit spec/factories/manager.rb

--- spec/factories/manager.rb
+++ spec/factories/manager.rb
@@ -1,12 +1,33 @@
+#  id                     :integer          not null, primary key
+#  phone                  :string(255)      default(""), not null
+#  encrypted_password     :string(255)      default(""), not null
+#  email                  :string(255)
+#  reset_password_token   :string(255)
+#  reset_password_sent_at :datetime
+#  remember_created_at    :datetime
+#  sign_in_count          :integer          default(0), not null
+#  current_sign_in_at     :datetime
+#  last_sign_in_at        :datetime
+#  current_sign_in_ip     :string(255)
+#  last_sign_in_ip        :string(255)
+#  created_at             :datetime         not null
+#  updated_at             :datetime         not null
+#  school_id              :integer
+#  type                   :string(255)
+#  tag                    :string(255)
+#  authentication_token   :string(30)
+#
+# Indexes
+#
+#  index_users_on_authentication_token  (authentication_token) UNIQUE
+#  index_users_on_phone                 (phone) UNIQUE
+#  index_users_on_reset_password_token  (reset_password_token) UNIQUE
+#  index_users_on_school_id             (school_id)
+#
+
 FactoryGirl.define do
   factory :manager do
     phone "13813813811"   
-    password "abcd.1234"
-    authentication_token "qwertyuiop123"
-  end
-
-  factory :manager1, class: Player do
-    phone "13813813812"   
     password "abcd.1234"
     authentication_token "qwertyuiop123"
   end
