b3016b7fcae7a4e70f4dc5e69261d88d7184fd8f
tangmingchao <18798805737@139.com>
2016-11-29 16:34:04 +0800

    edit spec/models/teacher_spec.rb

--- spec/models/teacher_spec.rb
+++ spec/models/teacher_spec.rb
@@ -1,5 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Teacher, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should belong_to(:school) }
+  it { should have_many(:training_course_teachers) }
+  it { should have_many(:appraises) }
 end
