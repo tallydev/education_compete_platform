e8bcd2d8cc3eda2ed9e3a591bf4ec74ceeadd5f7
tangmingchao <18798805737@139.com>
2016-11-29 09:07:36 +0800

    edit spec/models/manager_feedback_spec.rb

--- spec/models/manager_feedback_spec.rb
+++ spec/models/manager_feedback_spec.rb
@@ -1,5 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe ManagerFeedback, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should belong_to(:manager) }
+  it { should belong_to(:training_course) }
 end
