492f6b0d047e6df729c5a134f32fe038abbd57fd
tangmingchao <18798805737@139.com>
2016-11-29 16:49:16 +0800

    edit spec/models/training_course_teacher_spec.rb

--- spec/models/training_course_teacher_spec.rb
+++ spec/models/training_course_teacher_spec.rb
@@ -1,5 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe TrainingCourseTeacher, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should belong_to(:training_course) }
+  it { should belong_to(:teacher) }
 end
