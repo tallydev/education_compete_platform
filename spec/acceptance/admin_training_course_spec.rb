477936da5663aea87fccf62ffd9f838ff8193aff
tangmingchao <18798805737@139.com>
2016-11-30 16:39:39 +0800

    edit spec/acceptance/admin_training_course_spec.rb#审核部分的api suceess

--- spec/acceptance/admin_training_course_spec.rb
+++ spec/acceptance/admin_training_course_spec.rb
@@ -3,6 +3,26 @@ require 'acceptance_helper'
 resource "管理员 对 培训课程 内容的相关的API" do
   header "Accept", "application/json"
 
+  # #################### create #############################
+  # post '/admin/training_courses' do
+  #   admin_attrs = FactoryGirl.attributes_for(:admin)
+  #   training_course_attrs = FactoryGirl.attributes_for(:training_course)
+
+  #   header "X-Admin-Token", admin_attrs[:authentication_token]
+  #   header "X-Admin-Email", admin_attrs[:email]
+
+  #   before do
+  #     @admin = create(:admin)
+  #     @school = create(:school)
+  #   end
+
+  #   example "管理员 查询 培训课程信息 的列表成功" do
+  #     do_request
+  #     puts response_body
+  #     expect(status).to eq(201)
+  #   end
+  # end
+
   ############### before_do ################################
   describe 'training_course condition is all correct' do
     admin_attrs = FactoryGirl.attributes_for(:admin)
@@ -11,9 +31,9 @@ resource "管理员 对 培训课程 内容的相关的API" do
     header "X-Admin-Email", admin_attrs[:email]
 
     before do
-      @admin = create(:admin)
+      @admin = create(:admin) #登录
       @school = create(:school)
-      @training_courses = create_list(:training_course, 5, school: @school)
+      @training_courses = create_list(:training_course, 5, school: @school)#创建通过专家审核的
     end
 
     #################### index #############################
@@ -47,8 +67,8 @@ resource "管理员 对 培训课程 内容的相关的API" do
   	  end
     end
 
-    # ##################### show #############################
-    # get "/admin/training_courses/:id" do
+    # ##################### update #############################
+    # put "/admin/training_courses/:id" do
 
   	 #  let(:id) {@training_courses.first.id}
 
@@ -59,52 +79,60 @@ resource "管理员 对 培训课程 内容的相关的API" do
   	 #  end
     # end
 
-    # ##################### show #############################
-    # get "/admin/training_courses/:id" do
+    ##################### unchecked #############################
+    post "/admin/training_courses/:id/unchecked" do
 
-  	 #  let(:id) {@training_courses.first.id}
+  	  let(:id) {@training_courses.first.id}
 
-  	 #  example " 管理员 查询指定 培训课程信息 的详情成功" do
-  	 #    do_request
-  	 #    puts response_body
-  	 #    expect(status).to eq(201)
-  	 #  end
-    # end
+  	  example " 培训课程 未通过审核 的操作 成功" do
+  	    do_request
+  	    puts response_body
+  	    expect(status).to eq(201)
+  	  end
+    end
 
-    # ##################### show #############################
-    # get "/admin/training_courses/:id" do
+    ##################### checked_by_expert 专家审核 ###############
+    post "/admin/training_courses/:id/checked_by_expert" do
 
-  	 #  let(:id) {@training_courses.first.id}
+      before do #创建未通过审核的
+      	@unchecked_training_courses = create_list(:training_course, 5, school: @school, state: "unchecked")
+      end
 
-  	 #  example " 管理员 查询指定 培训课程信息 的详情成功" do
-  	 #    do_request
-  	 #    puts response_body
-  	 #    expect(status).to eq(201)
-  	 #  end
-    # end
+  	  let(:id) {@unchecked_training_courses.first.id}
 
-    # ##################### show #############################
-    # get "/admin/training_courses/:id" do
+  	  example " 培训课程 专家审核 的操作 成功" do
+  	    do_request
+  	    puts response_body
+  	    expect(status).to eq(201)
+  	  end
+    end
 
-  	 #  let(:id) {@training_courses.first.id}
+    ##################### checked_by_seminar 研究会审核 ################
+    post "/admin/training_courses/:id/checked_by_seminar" do
 
-  	 #  example " 管理员 查询指定 培训课程信息 的详情成功" do
-  	 #    do_request
-  	 #    puts response_body
-  	 #    expect(status).to eq(201)
-  	 #  end
-    # end
+  	  let(:id) {@training_courses.first.id}
 
-    # ##################### show #############################
-    # get "/admin/training_courses/:id" do
+  	  example " 培训课程 研究会审核 的操作 成功" do
+  	    do_request
+  	    puts response_body
+  	    expect(status).to eq(201)
+  	  end
+    end
 
-  	 #  let(:id) {@training_courses.first.id}
+    ##################### checked_by_educator 教委审核 #################
+    post "/admin/training_courses/:id/checked_by_educator" do
 
-  	 #  example " 管理员 查询指定 培训课程信息 的详情成功" do
-  	 #    do_request
-  	 #    puts response_body
-  	 #    expect(status).to eq(201)
-  	 #  end
-    # end
+      before do#创建研究会审核的
+      	@seminar_training_courses = create_list(:training_course, 5, school: @school, state: "checked_by_seminar")
+      end
+
+  	  let(:id) {@seminar_training_courses.first.id}
+
+  	  example " 培训课程 教委审核 的操作 成功" do
+  	    do_request
+  	    puts response_body
+  	    expect(status).to eq(201)
+  	  end
+    end
   end
 end
