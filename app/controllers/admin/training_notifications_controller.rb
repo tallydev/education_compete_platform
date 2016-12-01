7e54bb188038aa539309b6f9befdcca826217b15
tangmingchao <18798805737@139.com>
2016-12-01 11:50:44 +0800

    edit app/controllers/admin/training_notifications_controller.rb

--- app/controllers/admin/training_notifications_controller.rb
+++ app/controllers/admin/training_notifications_controller.rb
@@ -1,10 +1,14 @@
 class Admin::TrainingNotificationsController < ApplicationController
-  before_action :set_admin_training_notification, only: [:show, :edit, :update, :destroy]
+  acts_as_token_authentication_handler_for Admin 
+  before_action :set_admin_training_notification, only: [:show, :update]
 
-  respond_to :html
+  respond_to :json
 
   def index
-    @admin_training_notifications = Admin::TrainingNotification.all
+    @keyword = params[:keyword].present?? params[:keyword] : " "
+    page = params[:page] || 1
+    per_page = params[:per_page] || 15
+    @admin_training_notifications = TrainingNotification.all.find_keyword(@keyword).paginate(page: page, per_page: per_page)
     respond_with(@admin_training_notifications)
   end
 
@@ -12,36 +16,32 @@ class Admin::TrainingNotificationsController < ApplicationController
     respond_with(@admin_training_notification)
   end
 
-  def new
-    @admin_training_notification = Admin::TrainingNotification.new
-    respond_with(@admin_training_notification)
-  end
-
-  def edit
-  end
-
   def create
-    @admin_training_notification = Admin::TrainingNotification.new(admin_training_notification_params)
-    @admin_training_notification.save
-    respond_with(@admin_training_notification)
+    @admin_training_notification = TrainingNotification.new(admin_training_notification_params)
+    if @admin_training_notification.save
+      respond_with(@admin_training_notification)  
+    else
+      @error = "培训通知内容创建 失败 ！"
+      respond_with(@error, template: "error")
+    end
   end
 
   def update
     @admin_training_notification.update(admin_training_notification_params)
-    respond_with(@admin_training_notification)
+    respond_with(@admin_training_notification, template:"admin/training_notifications/show", status: 201)
   end
 
-  def destroy
-    @admin_training_notification.destroy
-    respond_with(@admin_training_notification)
-  end
+  # def destroy
+  #   @admin_training_notification.destroy
+  #   respond_with(@admin_training_notification)
+  # end
 
   private
     def set_admin_training_notification
-      @admin_training_notification = Admin::TrainingNotification.find(params[:id])
+      @admin_training_notification = TrainingNotification.find(params[:id])
     end
 
     def admin_training_notification_params
-      params[:admin_training_notification]
+      params.require(:training_notification).permit(:title, :sub_title, :view_count, :author, :content, :keyword)
     end
 end
