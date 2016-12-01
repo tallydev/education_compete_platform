fbb9cb094a043f6d6eb8e37075295dc6442d3e48
tangmingchao <18798805737@139.com>
2016-12-01 16:47:50 +0800

    edit spec/models/player_training_course_spec.rb

--- spec/models/player_training_course_spec.rb
+++ spec/models/player_training_course_spec.rb
@@ -1,5 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe PlayerTrainingCourse, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should belong_to(:player) }
+  it { should belong_to(:training_course) }
+  # it { should belong_to(:divide) }
 end
