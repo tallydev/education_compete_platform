58e53d0b87166272aa429269cd5556b889bfbf6f
tangmingchao <18798805737@139.com>
2016-12-01 10:39:48 +0800

    edit spec/models/training_notification_spec.rb

--- spec/models/training_notification_spec.rb
+++ spec/models/training_notification_spec.rb
@@ -1,5 +1,5 @@
 require 'rails_helper'
 
 RSpec.describe TrainingNotification, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should have_one(:training_course) } 
 end
