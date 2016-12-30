6a5affddfc111aff098a8b169c68aaaaf7e185b4
iboying <weboying@gmail.com>
2016-12-30 17:10:18 +0800

    add auth for opuses

--- app/controllers/opuses_controller.rb
+++ app/controllers/opuses_controller.rb
@@ -1,4 +1,7 @@
 class OpusesController < ApplicationController
+	before_action :authenticate_expert!, only: [:index], if: -> {!(current_expert.present? || current_school_user.present?)}
+	before_action :authenticate_school_user!, only: [:index], if: -> {!(current_expert.present? || current_school_user.present?)}
+
 	def index
 	  @activities = Activity.onlines
 	  activity_id = params[:activity_id] || @activities.first.id
