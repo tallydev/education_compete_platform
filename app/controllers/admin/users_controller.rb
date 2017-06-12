599afb086dfd7c53a9e58c0f3aadc59564562520
iboying <weboying@gmail.com>
2017-06-12 16:38:51 +0800

    Improve submit text and admin logic

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,15 +1,19 @@
 module Admin
   class UsersController < BaseController
-    
+    before_action :set_user, only: [:destroy]
+
     def index
       @users = Player.all.paginate(:page => params[:page], :per_page => 20)
     end
 
     def destroy
-      
+      @user.destroy
+      redirect_to admin_users_path
     end
 
     private
-    
+      def set_user
+        @user = Player.find params[:id]
+      end
   end
 end
